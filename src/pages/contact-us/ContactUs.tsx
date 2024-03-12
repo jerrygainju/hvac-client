9e0911702ae6dacec16f0e986da213e47e87b1f6
unknown <ASUS@SG-LAP-12.sgdesign.com>
2024-03-12 15:02:07 +0545

    fixes

--- src/pages/contact-us/ContactUs.tsx
+++ src/pages/contact-us/ContactUs.tsx
@@ -54,20 +54,20 @@ const ContactUs = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col w-4/12 mt-8 p-6  bg-white border rounded-md shadow-xl">
+      <div className="flex flex-col w-4/12 mt-8 p-6  bg-white border rounded-md shadow-2xl">
         <h2 className="text-2xl font-semibold mb-4">Get in Touch</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label htmlFor="name" className="block text-gray-600 font-medium">Your Name</label>
-            <Input type="text" id="name" name="name" value={formData.name} onChange={handleChange} className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-blue-500" />
+            <Input type="text" id="name" name="name" value={formData.name} onChange={handleChange} className="mt-1 p-2 w-full border rounded-md shadow-lg focus:outline-none focus:border-blue-500" />
           </div>
           <div>
             <label htmlFor="email" className="block text-gray-600 font-medium">Your Email</label>
-            <Input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-blue-500" />
+            <Input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="mt-1 p-2 w-full border rounded-md shadow-lg focus:outline-none focus:border-blue-500" />
           </div>
           <div>
             <label htmlFor="message" className="block text-gray-600 font-medium">Your Message</label>
-            <TextArea id="message" name="message" value={formData.message} onChange={handleChange} className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-blue-500"></TextArea>
+            <TextArea id="message" name="message" value={formData.message} onChange={handleChange} className="mt-1 p-2 w-full border rounded-md shadow-lg focus:outline-none focus:border-blue-500"></TextArea>
           </div>
           <button type="submit" className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-700">Submit</button>
         </form>
